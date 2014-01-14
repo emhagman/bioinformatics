deaf501846d47d63bee08c15244a61af9581be8e
Eric Hagman <emhagman@gmail.com>
2014-01-14 14:45:30 -0500

    mrna, etc

--- sequencing/dna.py
+++ sequencing/dna.py
@@ -4,7 +4,22 @@ class DNA:
         print('static class, should not see this')
 
     @staticmethod
+    def string_to_list(seq):
+        return [s for s in seq]
+
+    @staticmethod
+    def mrna(dna):
+        if 'string' in type(dna):
+            dna = dna.lower().replace('a', 'u')
+            return DNA.string_to_list(dna)
+        else:
+            dna = DNA.string_to_list(dna)
+            return map(DNA.mrna_map, dna)
+
+    @staticmethod
     def complement(seq):
+        if 'string' in type(seq):
+            seq = DNA.string_to_list(seq)
         return map(DNA.complement_map, seq)
 
     @staticmethod
@@ -18,3 +33,11 @@ class DNA:
             return 'g'
         elif 'g' in marker:
             return 'c'
+
+    @staticmethod
+    def mrna_map(marker):
+        marker = marker.lower()
+        if 'a' in marker:
+            return 'u'
+        else:
+            return marker
